4d17b432ed690c797bd9b4899a5f1f584746f2f4
Yeimss <jamesherrera364@gmail.com>
2025-04-23 22:59:54 -0500

    Inyección de dependencias de Categorias y Tareas

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareasService, TareasService>();
 
 var app = builder.Build();
 
